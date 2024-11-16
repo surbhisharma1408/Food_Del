6b83601feb36a5edb7d18eda2444c315eab4690d
surbhisharma1408 <144913805+surbhisharma1408@users.noreply.github.com>
2024-11-16 16:42:41 +0530

    Add files via upload

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react'
+import Navbar from './Components/Navbar/Navbar'
+import { Route, Routes } from 'react-router-dom'
+import Home from './Pages/Home/Home'
+import Cart from './Pages/Cart/Cart'
+import PlaceOrder from './Pages/PlaceOrder/PlaceOrder'
+import LoginPopup from './Components/LoginPopup/LoginPopup'
+
+const App = () => {
+  const [showLogin,setShowLogin] = useState(false);
+
+  return (
+    <>
+   { showLogin?<LoginPopup setShowLogin={setShowLogin}/>:<></>}
+    <div className='app'>
+      <Navbar setShowLogin={setShowLogin}/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/placeorder' element={<PlaceOrder/>}/>
+       
+      </Routes>
+      
+    </div>
+    </>
+  )
+}
+
+export default App
